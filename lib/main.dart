a8f2bd35a292d6787bf181acb4da6b06d9dfd582
sahil <sahil.kumar@geeky.dev>
2025-10-23 17:33:01 +0530

    added more changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:workout_app/screens/Login.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const  ProviderScope(child: WorkoutApp()));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(const ProviderScope(child: WorkoutApp()));
 }
 
 class WorkoutApp extends StatelessWidget {
@@ -14,11 +20,8 @@ class WorkoutApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Workout App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  LoginScreen(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
     );
   }
 }
-
